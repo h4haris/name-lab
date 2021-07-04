7fde8fe4be4a0dec4199f63c8040525a1cb30858
Shaikh, Muhammad Haris <muhammadharis.shaikh@ncr.com>
2021-07-04 11:24:58 +0500

    Fix for result container not becoming empty

--- src/components/ResultsContainer/ResultsContainer.jsx
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -3,8 +3,8 @@ import NameCard from '../NameCard/NameCard';
 import './ResultsContainer.css';
 
 const ResultsContainer = ({ suggestedNames }) => {
-    const suggestNameJsx = suggestedNames.map((suggestedName) => {
-        return <NameCard key={suggestedName} suggestedName={suggestedName} />;
+    const suggestNameJsx = suggestedNames.map((suggestedName,index) => {
+        return <NameCard key={index} suggestedName={suggestedName} />;
     });
 
     return <div className="results-container">{suggestNameJsx}</div>;
